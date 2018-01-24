2023859a90ac1bb301655f7e6355890f881c023c
xmfenbei <xmfenbei@163.com>
2018-01-24 23:36:12 +0800

    Initial Project

---
+++ demo/demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by fenbei on 2018/1/24.
+//  Copyright © 2018年 xmfenbei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by fenbei on 2018/1/24.
+//  Copyright © 2018年 xmfenbei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
